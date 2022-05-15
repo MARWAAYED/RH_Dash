72c45a92e32b4ff318706116ac5f8b930133d85a
marwa <marwaayed74@gmail.com>
2022-05-15 21:19:45 +0100

    dashboard p1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -13,9 +15,19 @@ class HomeController extends AbstractController
      *@Route("/" , name="homepage")
      *
      */
-    public function home(){
+    public function home(EntityManagerInterface $manager){
+
+        $employee = $manager->createQuery('SELECT COUNT(e) FROM App\Entity\Employee e')->getSingleScalarResult();
+        $applicant = $manager->createQuery('SELECT COUNT(ap) FROM App\Entity\Applicant ap')->getSingleScalarResult();
+
+        //dump($employee ,$applicant);
+
+        $contract = $manager->createQuery('SELECT COUNT(c) FROM App\Entity\Contract c')->getSingleScalarResult();
+        
+        dump($contract);
+        
         return $this->render('base.html.twig', [
-            'controller_name' => 'HomeController',
+            'stats' => Compact('employee', 'applicant')
         ]);
     }
 
