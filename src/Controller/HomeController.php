2b5e0565c1de5e480b644324429375d964ee8976
marwa <marwaayed74@gmail.com>
2022-05-16 23:22:13 +0100

    dashboard p2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,9 +22,29 @@ class HomeController extends AbstractController
 
         //dump($employee ,$applicant);
 
-        $contract = $manager->createQuery('SELECT COUNT(c) FROM App\Entity\Contract c')->getSingleScalarResult();
+        $job = $manager->createQuery('SELECT COUNT(j) FROM App\Entity\Job j')->getSingleScalarResult();
+        $contract = $manager->createQuery('SELECT COUNT(t) FROM App\Entity\Contract t')->getSingleScalarResult();
+
+        $wage = $manager->createQuery('SELECT AVG(t.wage) as moy FROM App\Entity\Contract t')->getSingleScalarResult();
         
-        dump($contract);
+        $jobt = $manager->createQuery('SELECT e.job_title  FROM App\Entity\Employee e GROUP BY e.job_title')->getResult();
+        
+        $jobtitl = $manager->createQuery('SELECT e.job_title, COUNT(e) FROM App\Entity\Employee e GROUP BY e.job_title')->getResult();
+
+        $nbjobt = $manager->createQuery('SELECT e.job_title, AVG(e.id) FROM App\Entity\Employee e GROUP BY e.job_title')->getResult();
+
+        $moy_abbs = $manager->createQuery('SELECT l.private_name, AVG(l.number_of_days) FROM App\Entity\Leave l GROUP BY l.private_name')->getResult();
+
+        $nbjour = $manager->createQuery('SELECT AVG(l.number_of_days) FROM App\Entity\Leave l WHERE (l.number_of_days)<2 ')->getResult();
+        
+        $nbj = $manager->createQuery('SELECT AVG(l.number_of_days) FROM App\Entity\Leave l WHERE (l.number_of_days)>3 ')->getResult();
+
+        $moyjob = $manager->createQuery('SELECT COUNT(e.job_title) FROM App\Entity\Employee e GROUP BY e.job_title')->getResult();
+
+        $MOY = $manager->createQuery('SELECT Avg(COUNT(ap.name)) FROM App\Entity\Applicant ap GROUP BY ap.stage')->getResult();
+
+
+        dump($MOY);
         
         return $this->render('base.html.twig', [
             'stats' => Compact('employee', 'applicant')
